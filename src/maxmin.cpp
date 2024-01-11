7d2585824bf70e8e67fdf7c5ad4c585e82b9dfd5
KinChow <315458934@qq.com>
2024-01-12 02:41:09 +0800

    Add demo (manipulation, maxmin, mla, mls, mul, negative, reciprocal, round, shift, sub)

--- src/maxmin.cpp
+++ src/maxmin.cpp
@@ -6,19 +6,35 @@
 void demo_vmax_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vcreate_s8(0x0f0e0d0c04030201);
+    int8x8_t v2_s8 = vcreate_s8(0x080706050b0a0908);
+    int8x8_t v3_s8 = vmax_s8(v1_s8, v2_s8);
+    print_vector(v3_s8);
 }
 
 void demo_vpmax_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vcreate_s8(0x0f0e0d0c04030201);
+    int8x8_t v2_s8 = vcreate_s8(0x080706050b0a0908);
+    int8x8_t v3_s8 = vpmax_s8(v1_s8, v2_s8);
+    print_vector(v3_s8);
 }
 
 void demo_vmin_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vcreate_s8(0x0f0e0d0c04030201);
+    int8x8_t v2_s8 = vcreate_s8(0x080706050b0a0908);
+    int8x8_t v3_s8 = vmin_s8(v1_s8, v2_s8);
+    print_vector(v3_s8);
 }
 
 void demo_vpmin_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vcreate_s8(0x0f0e0d0c04030201);
+    int8x8_t v2_s8 = vcreate_s8(0x080706050b0a0908);
+    int8x8_t v3_s8 = vpmin_s8(v1_s8, v2_s8);
+    print_vector(v3_s8);
 }
--- src/negative.cpp
+++ src/negative.cpp
@@ -6,9 +6,15 @@
 void demo_vneg_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vcreate_s8(0x0807060504030201);
+    int8x8_t v2_s8 = vneg_s8(v1_s8);
+    print_vector(v2_s8);
 }
 
 void demo_vqneg_type()
 {
     std::cout << __FUNCTION__ << std::endl;
+    int8x8_t v1_s8 = vdup_n_s8(-128);
+    int8x8_t v2_s8 = vqneg_s8(v1_s8);
+    print_vector(v2_s8);
 }
